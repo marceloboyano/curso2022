1f9ecea693f558e25d87e5db35d2c04b05df0e02
marcelo boyanosky <marceloboyanosky@gmail.com>
2022-05-06 05:09:55 -0300

    clase serie

---
+++ clase4-condicionales/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,79 @@
+﻿//Ejemplo de condicional if
+
+//Console.WriteLine("Ingrese su edad: ");// le pedimos al usuario que ingrese su edad
+//int edad = Convert.ToInt32(Console.ReadLine());// obtenemos la edad por teclado
+
+////Console.WriteLine("Ingrese su equipo de futbol: ");
+////string equipo = Console.ReadLine();
+//Console.WriteLine("La edad ingresada es: " + edad);
+
+////if (edad < 18 && equipo == "River")//evaluamos la edad y equipo
+//if(edad <18)
+//{
+//    //Instrucciones si se cumple
+//    Console.WriteLine("Usted es menor de edad.");
+//}
+//else
+//{
+//    //Instrucciones si no se cumple la condicion evaluada
+//    Console.WriteLine("Usted es mayor de edad.");
+//}
+
+//Ejemplo if-else if-else- else
+//Console.Write("Ingrese la edad: ");
+//int edad = Convert.ToInt32(Console.ReadLine());
+//if (edad < 0 || edad > 100)
+//{
+//    Console.WriteLine("La edad no es valida.");
+//}
+//else if(edad < 12)
+//{
+//    Console.WriteLine("Va a la primaria");
+//} else if (edad < 18)
+//{
+//    Console.WriteLine("Va al secundario");
+//} else if(edad < 28)
+//{
+//    Console.WriteLine("Va a la facultad");
+
+//}else
+//{
+//    Console.WriteLine("A trabajar");
+//}
+
+//Ejemplo del Switch
+Console.WriteLine("Ingrese el primer numero: ");
+double numero1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el segundo numero: ");
+double numero2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("1- suma");
+Console.WriteLine("2- resta");
+Console.WriteLine("3- multiplicacion");
+Console.WriteLine("4- division");
+Console.WriteLine("Ingrese la operacion a realizar");
+string operacion = Console.ReadLine();
+double resultado =0;
+switch (operacion)
+{
+    case "1":
+        Console.WriteLine("Sumando...");
+        resultado = numero1 + numero2;     
+        break;
+    case "2":
+        Console.WriteLine("Restando...");
+        resultado = numero1 - numero2;
+        break;
+    case "3":
+        Console.WriteLine("multiplicando...");
+        resultado = numero1 * numero2;
+        break;
+    case "4" when numero2 != 0:
+        Console.WriteLine("dividiendo...");
+        resultado = numero1 / numero2;
+        break;
+    default:
+        Console.WriteLine("opcion no valida");
+        break;
+}
+Console.WriteLine("El resultado es: "+resultado);
---
+++ practicando/ejercicio/ejercicio/Serie.cs
@@ -0,0 +1,47 @@
+﻿
+serie walkingdead = new serie("peterpan",4,"accion","travolta");
+Console.WriteLine(walkingdead.ToString());
+Console.WriteLine("titulo: " + walkingdead.Titulo + " numero de temporadas: " +walkingdead.NumeroTemporadas+ " genero: "+walkingdead.Genero+" creador: "+walkingdead.Creador);
+public class serie
+{
+    private string titulo;
+    private int numeroTemporadas = 3;
+    private bool entregado;
+    private string genero;
+    private string creador;
+
+    public serie(string titulo, int numeroTemporadas, string genero, string creador)
+    {
+        this.titulo = titulo;
+        this.numeroTemporadas = numeroTemporadas;
+        entregado = false;
+        this.genero = genero;
+        this.creador = creador;
+
+
+    }
+    public serie(string titulo, string creador)
+    {
+        this.titulo = titulo;
+        this.creador = creador;
+
+    }
+
+    public serie()
+    {
+    }
+
+    public string Titulo { get => titulo; set => titulo = value; }
+    public int NumeroTemporadas { get => numeroTemporadas; set => numeroTemporadas = value; }
+    public string Genero { get => genero; set => genero = value; }
+    public string Creador { get => creador; set => creador = value; }
+
+    public override string ToString() => $"titulo: {titulo} numero de temporadas: {numeroTemporadas}  genero: {genero} creador: {creador}";
+
+
+
+}
+
+
+
+
