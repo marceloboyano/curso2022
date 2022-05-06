24f8c345beee131898f460ed9baead16d2738f7f
marcelo boyanosky <marceloboyanosky@gmail.com>
2022-05-06 05:17:13 -0300

    agregue la consigna

--- practicando/ejercicio/ejercicio/Serie.cs
+++ practicando/ejercicio/ejercicio/Serie.cs
@@ -1,4 +1,14 @@
-﻿
+﻿/*Crearemos una clase llamada Serie con las siguientes caracteristicas: Sus atributos son titulo, numero de temporadas, entregado, genero y creador.
+ * Por defecto, el numero de temporadas es de 3 temporadas y entregado false.
+ * El resto de atributos serán valores por defecto según el tipo de atributo. 
+ * Los constructores que se implementaran serán:
+ * Un constructor por defecto.
+ * Un constructor con el titulo y creador.
+ * El resto por defecto. Un constructor con todos los atributos, excepto de entregado.
+ * Los metodos que se implementara serán:
+ * Métodos get de todos los atributos, excepto de entregado.
+ * Metodos set de todos los atributos, excepto entregado. Sobreescribe los metodos toString. */
+
 serie walkingdead = new serie("peterpan",4,"accion","travolta");
 Console.WriteLine(walkingdead.ToString());
 Console.WriteLine("titulo: " + walkingdead.Titulo + " numero de temporadas: " +walkingdead.NumeroTemporadas+ " genero: "+walkingdead.Genero+" creador: "+walkingdead.Creador);
