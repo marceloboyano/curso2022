19667ea3e8fc6d95ff7ec639cfcea186fa587400
marcelo boyanosky <marceloboyanosky@gmail.com>
2022-05-07 08:26:20 -0300

    ejercicio con validaciones basicas

---
+++ practicando/fizzbuzz/fizzbuzz/fizzbuzz/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 3 ==0)
+    {
+        Console.WriteLine("FIZZ");
+        if (i % 5 == 0)
+        {
+            Console.WriteLine("FIZZBUZZ");
+        }
+
+    }
+    else if(i % 5 == 0)
+    {
+        Console.WriteLine("buzz");
+    }else
+    {
+        Console.WriteLine("numero: " + i);
+    }
+}
---
+++ practicando/validaciones/validaciones/validaciones/Program.cs
@@ -0,0 +1,56 @@
+﻿
+
+
+int inferior;
+int superior;
+bool validI;
+bool validS;
+
+Console.Write("Ingrese el limite inferior de tipo entero: ");
+ validI = int.TryParse(Console.ReadLine(), out inferior);
+if (!validI)
+{
+    Console.WriteLine("No ha ingresado un numero entero");
+    return;
+} 
+Console.Write("Ingrese el limite Superior de tipo entero: ");
+ validS = int.TryParse(Console.ReadLine(), out superior);
+if (!validS)
+{
+    Console.WriteLine("No ha ingresado un numero entero:");
+    return;
+
+}
+if (inferior < 0 && superior < 0)
+{
+    Console.Write("Los limites ingresados son incorrectos, no pueden ser ni 0 ni un numeros negativo:" +inferior +","+superior);
+    return;
+}
+if (superior < inferior)
+{
+    Console.Write("El limite superior no puede ser menor que el limite inferior:" + inferior + "," + superior);
+    return;
+}
+
+
+
+for (int i = inferior; inferior <= 100; i++)
+{
+    if (i % 3 == 0)
+    {
+        Console.WriteLine("FIZZ");
+        if (i % 5 == 0)
+        {
+            Console.WriteLine("FIZZBUZZ");
+        }
+
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine("buzz");
+    }
+    else
+    {
+        Console.WriteLine("numero: " + i);
+    }
+}
