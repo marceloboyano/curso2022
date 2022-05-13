f6cba67e4de5c9f2646cee693ba54ab41e70d5e1
marcelo boyanosky <marceloboyanosky@gmail.com>
2022-05-13 05:58:25 -0300

    ejercicio numero secreto

---
+++ clase6/ejercicioClase6/numeroSecreto/numero/numerosecreto/numerosecreto/Program.cs
@@ -0,0 +1,24 @@
+﻿int numeroSecreto = new Random(DateTime.Now.Millisecond).Next(1,21);
+int numeroIngresado;
+int contador = 0;
+do
+{
+    Console.Write("Ingrese un número del 1 al 20 e intente adivinar el que eligió la computadora: ");
+    numeroIngresado = int.Parse(Console.ReadLine());
+    contador++;
+    if (numeroIngresado <= numeroSecreto)
+    {
+        if (numeroIngresado < numeroSecreto)
+        {
+            Console.WriteLine("EL número ingresado es menor al numero secreto. Vuelva a intentarlo");
+        }
+        else
+        {
+            Console.WriteLine($"Felicitaciones, has adivinado el número secreto que era: [{ numeroSecreto}] \nLo has logrado en [{contador} intentos!!]");
+        }
+    }
+    else
+    {
+        Console.WriteLine("EL número ingresado es mayor al numero secreto. Vuelva a intentarlo");
+    }
+} while(numeroIngresado != numeroSecreto);
