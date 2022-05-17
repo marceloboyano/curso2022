67f6b62d214a9c34614184c623c5d4bc19a9e30f
marcelo boyanosky <marceloboyanosky@gmail.com>
2022-05-16 21:21:28 -0300

    ultimo de fizzbuzz

---
+++ practicando/validaciones/validaciones/validaciones/FizzBuzz.cs
@@ -0,0 +1,70 @@
+﻿
+
+namespace validaciones
+{
+    public class FizzBuzz
+    {
+
+        public int Inferior { get; set; }
+        public int Superior { get; set; }
+        public FizzBuzz() { }
+       public FizzBuzz(int _inferior, int _superior)
+        {
+
+            if (!Validaciones(_inferior, _superior))
+            {
+                Console.WriteLine("Los Datos ingresados son incorrectos." +
+                    "\n* No se puede ingresar numeros negativos" +
+                    " \n* El limite superior no puede ser menor que el limite inferior" +
+                    " \n* los limites no pueden ser mayores que 10mil");
+            }
+            else
+            {
+                for (int i = _inferior; i <= _superior; i++)
+                {
+                    if (i % 3 != 0 || i % 5 != 0)
+                    {
+                        if (i % 5 == 0 || i % 3 == 0)
+                        {
+                            if (i % 5 != 0)
+                            {
+                                                             
+                                Console.WriteLine("FIZZ");
+                                
+                            }
+                            else
+                            {
+                                Console.WriteLine("BUZZ");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("numero: " + i);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("FIZZBUZZ");
+
+                    }
+
+                }
+            }
+
+        }
+
+       public bool Validaciones(int inferior, int superior)
+        {
+
+
+            if (inferior < 0 || superior < 0 || superior < inferior || superior > 10000 || inferior > 10000)
+            {
+                return false;
+            }
+            return true;
+
+
+        }
+
+    }
+}
--- practicando/validaciones/validaciones/validaciones/Program.cs
+++ practicando/validaciones/validaciones/validaciones/Program.cs
@@ -1,6 +1,8 @@
 
 
 
+using validaciones;
+
 int inferior;
 int superior;
 bool validI;
@@ -8,88 +10,17 @@ bool validS;
 
 Console.Write("Ingrese el limite inferior de tipo entero: ");
 validI = int.TryParse(Console.ReadLine(), out inferior);
-if (!validI)
-{
-    Console.WriteLine("No ha ingresado un numero entero");
-    return;
-}
-
 Console.Write("Ingrese el limite Superior de tipo entero: ");
 validS = int.TryParse(Console.ReadLine(), out superior);
 
-
-if (!validS)
+if (!(!validI || !validS || !validS && validI))
 {
-    Console.WriteLine("No ha ingresado un numero entero:");
-    return;
-
-}
-
-Fizzbuzz(inferior,superior);
-
-
- bool Validaciones(int inferior, int superior)
-{
-
-
-    if (inferior < 0)
-    {      
-        return false;
-    }
-    else if (superior < 0)
-    {
-        return false;
-    }
-
-    if (superior < inferior)
-    {
-      return false;
-    }
-    if(inferior > 10000)
-    {
-        return false;
-    }else if (superior > 10000)
-    {
-        return false;
-    }
-    return true;
-
-
-}
-void Fizzbuzz(int inferior, int superior)
-{
-
-    if ((Validaciones(inferior, superior))) 
-    {
-
-
-        for (int i = inferior; i <= superior; i++)
-        {
-            if (i % 3 == 0 && i % 5 == 0)
-            {
-                Console.WriteLine("FIZZBUZZ");
-
-            }
-            else if (i % 5 != 0 && i % 3 != 0)
-            {
-                Console.WriteLine("numero: " + i);
-            }
-            else if (i % 5 == 0)
-            {
-                Console.WriteLine("BUZZ");
-            }
-            else if (i % 3 == 0)
-            {
-                Console.WriteLine("FIZZ");
-            }
-
-        }
-    }else
-    {
-        Console.WriteLine("Los Datos ingresados son incorrectos." +
-            "\n* No se puede ingresar numeros negativos" +
-            " \n* El limite superior no puede ser menor que el limite inferior" +
-            " \n* los limites no pueden ser mayores que 10mil");
-    }
+    FizzBuzz Fito = new FizzBuzz(inferior, superior);
+   
     
 }
+
+Console.WriteLine("No ha ingresado valores enteros en los limites");
+
+
+
