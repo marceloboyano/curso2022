f5ec55c3d191cfc1f9f4dc4b5c1603a5e9fb3f52
marcelo boyanosky <marceloboyanosky@gmail.com>
2022-05-18 17:05:51 -0300

    optimice con 2 for

--- clase 7/ejercicio clase7/mayor/mayor/Program.cs
+++ clase 7/ejercicio clase7/mayor/mayor/Program.cs
@@ -5,15 +5,15 @@ Console.WriteLine("Ingrese los 10 numeros que formaran el array");
 int[] numeros = new int[10];
 for (int i = 0; i < numeros.Length; i++)
 {
-    Console.WriteLine($"Ingrese el valor: {i+1}");
-     valid = int.TryParse(Console.ReadLine(), out numeros[i]);
+    Console.WriteLine($"Ingrese el valor: {i + 1}");
+    valid = int.TryParse(Console.ReadLine(), out numeros[i]);
 
     //Hago unas validaciones para que no ingrese cualquier cosa
     if (valid == false || numeros[i] < 0)
     {
         Console.WriteLine("Los numeros del array deben ser enteros positivos. \nPresione cualquier tecla para Salir");
         Console.ReadKey();
-        return;       
+        return;
     }
 }
 //le asigno el primer valor del array a menor y mayor
@@ -23,24 +23,22 @@ int menor = numeros[0];
 //porque sino las operaciones entre enteros devuelven enteros
 double suma = 0;
 
-
+Console.Write("Mostrando los numeros del array ingresados: ");
 for (int i = 0; i < numeros.Length; i++)
 {
     suma += numeros[i];
     if (numeros[i] > mayor) mayor = numeros[i];
-    if (numeros[i] < menor) menor = numeros[i];   
-   
+    if (numeros[i] < menor) menor = numeros[i];
+    Console.Write($"{numeros[i]}, ");
+
 }
 //ahora si lo redondeo al promedio
 double promedio = Math.Round(suma / numeros.Length);
-Console.Write("Mostrando los numeros del array ingresados: ");
-for (int i = 0; i < numeros.Length; i++)
-{
-    Console.Write($"{numeros[i]}, ");
-}
+
 // lo pongo asi para que se entienda 
 Console.WriteLine($"\n La suma de todos los números ingresados es : {suma} " +
     $"\n El numero mayor del array es: {mayor} " +
     $"\n el numero menor del array es: {menor} " +
     $"\n El promedio de todos los numeros del array es: {promedio} ");
+
 
