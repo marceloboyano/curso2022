512dbe49a3db3df0662934f8679e97e2fb322d8e
marcelo boyanosky <marceloboyanosky@gmail.com>
2022-05-18 08:22:08 -0300

    clase 7 y ejercicio

---
+++ clase 7/ejercicio clase7/mayor/mayor/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+bool valid;
+Console.WriteLine("Ingrese los 10 numeros que formaran el array");
+int[] numeros = new int[10];
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine($"Ingrese el valor: {i+1}");
+     valid = int.TryParse(Console.ReadLine(), out numeros[i]);
+
+    //Hago unas validaciones para que no ingrese cualquier cosa
+    if (valid == false || numeros[i] < 0)
+    {
+        Console.WriteLine("Los numeros del arreglo deben ser enteros positivos. \nPresione cualquier tecla para Salir");
+        Console.ReadKey();
+        return;       
+    }
+}
+int mayor = numeros[0];
+int menor = numeros[0];
+//pongo suma como double porque al sacar el promedio debe haber un numero double
+//porque sino las operaciones entre enteros devuelven enteros
+double suma = 0;
+
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    suma += numeros[i];
+    if (numeros[i] > mayor) mayor = numeros[i];
+    if (numeros[i] < menor) menor = numeros[i];   
+   
+}
+//ahora si lo redondeo al promedio
+double promedio = Math.Round(suma / numeros.Length);
+Console.Write("Mostrando los numeros del array ingresados: ");
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.Write($"{numeros[i]}, ");
+}
+// lo pongo asi para que se entienda 
+Console.WriteLine($"\n La suma de todos los números ingresados es : {suma} " +
+    $"\n El numero mayor del array es: {mayor} " +
+    $"\n el numero menor del array es: {menor} " +
+    $"\n El promedio de todos los numeros del array es: {promedio} ");
+
---
+++ clase 7/matrices/matrices/Program.cs
@@ -0,0 +1,47 @@
+﻿
+string[] cars = { "Ford", "Fiat", "BMW", "Toyota" };
+
+Console.WriteLine(cars[0]);
+
+string[] frutas = new string[4];
+
+frutas[0] = "Pera";
+frutas[1] = "Manzana";
+frutas[2] = "Banana";
+frutas[2] = "Kiwi";
+
+
+Console.WriteLine(frutas[2]);
+Console.WriteLine(frutas[3]);// devuelve vacio por ser definido como string -NO ES NULL-
+
+int [] numeros = new int[5];
+Console.WriteLine($"el array tiene: {numeros.Length} posiciones");
+
+//Console.WriteLine("Ingrese los valores de la matriz");
+
+//Ints[0] =  int.Parse(Console.ReadLine());
+//Ints[1] = int.Parse(Console.ReadLine());
+//Ints[2] = int.Parse(Console.ReadLine());
+//Ints[3] = int.Parse(Console.ReadLine());
+//Ints[4] = int.Parse(Console.ReadLine());
+
+for (int i = 0;i < numeros.Length; i++)
+{
+    Console.WriteLine($"Ingrese el numero: {i+1} ");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+int suma = 0;
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    suma += numeros[i];
+}
+
+Console.WriteLine("Los valores son:");
+for (int i = 0; i < numeros.Length; i++)
+{
+
+    Console.Write(numeros[i]+",");
+    
+}
+Console.WriteLine("La suma de todos los valores es: " + suma);
